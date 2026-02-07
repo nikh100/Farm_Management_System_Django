99a1257b11b66e940e3534eb23936c0defc7a94e
NIKHIL <nikh9411@gmail.com>
2026-02-08 03:26:39 +0530

    Fix CSRF for railway domain

--- FarmManagementSystem/settings.py
+++ FarmManagementSystem/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['farm-management-system-47wm.onrender.com', 'localhost', '127.0.0.1', '*']
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://farmmanagementsystemdjango-production.up.railway.app"
+]
+
 
 
 # Application definition
